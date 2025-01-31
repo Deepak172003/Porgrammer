3ad0b67feb86207a3b8ad556835ed07020adb6bd
Deepak Kumar Singh <deepakkumarsingh172003@gmail.com>
2025-01-31 23:54:39 +0530

    Add files via upload

---
+++ practice 2.py
@@ -0,0 +1,50 @@
+a =  int (input("Enter number 1 :"))
+b =  int (input ("Enter number 2 :"))
+
+sum = a+b
+print("Sum:",a+b)
+
+# remainder when divided by 2
+
+a = float (input("Enter number which is divided by 2 :"))
+
+Y = a%2
+
+print("Y:",Y) 
+
+#  check type of variable assigend using input function
+
+a = "Deepak"
+type(a)
+print(type(a))
+
+# comparison of two number
+
+a = 34
+b = 80
+
+Y = a>b
+
+print("Y :", a>b)
+
+
+# find average of number
+
+a = float(input("Enter number1 :"))
+b = float (input("Enter number2 :"))
+c = float (input("Entr number 3 :"))
+d = float(input("Enter number 4 :"))
+e = float(input("Enter number 5 :"))
+
+average = (a+b+c+d) / 5 
+
+print("average :", (a+b+c+d+e) /5)
+
+# calculate square of a number
+
+a = float(input("Enter number 1 :"))
+b = float (input("Enter number 2 :"))
+
+square = a*b
+
+print(square)
